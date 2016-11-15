ad6ee466914e82244740be63fc2172bbad3bfa0b
nairod66 <soixante6dollars@gmail.com>
2016-11-15 14:26:51 -0500

    Add files via upload

--- tictactoe.sql
+++ tictactoe.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Dim 06 Novembre 2016 à 12:52
+-- Généré le :  Mar 15 Novembre 2016 à 19:26
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -19,29 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `tictactoe`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `partie`
---
-
-CREATE TABLE `partie` (
-  `id` int(11) NOT NULL,
-  `joueur1` varchar(30) NOT NULL,
-  `joueur2` varchar(30) NOT NULL,
-  `couleur_joueur1` varchar(1) NOT NULL,
-  `main` varchar(30) NOT NULL,
-  `00` varchar(1) NOT NULL,
-  `01` varchar(1) NOT NULL,
-  `02` varchar(1) NOT NULL,
-  `10` varchar(1) NOT NULL,
-  `11` varchar(1) NOT NULL,
-  `12` varchar(1) NOT NULL,
-  `20` varchar(1) NOT NULL,
-  `21` varchar(1) NOT NULL,
-  `22` varchar(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -69,26 +46,11 @@ INSERT INTO `utilisateur` (`pseudo`, `motdepasse`) VALUES
 --
 
 --
--- Index pour la table `partie`
---
-ALTER TABLE `partie`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD UNIQUE KEY `pseudo` (`pseudo`);
 
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `partie`
---
-ALTER TABLE `partie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
