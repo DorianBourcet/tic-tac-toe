67fc622629d7d89d19190301e824af7296e4ebfa
nairod66 <soixante6dollars@gmail.com>
2016-11-17 08:05:39 -0500

    Add files via upload

--- src/java/com/jcfdb/listeners/EcouteurSession.java
+++ src/java/com/jcfdb/listeners/EcouteurSession.java
@@ -7,8 +7,10 @@
 package com.jcfdb.listeners;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
@@ -19,12 +21,17 @@ import javax.servlet.http.HttpSessionListener;
  * @author moumene
  */
 public class EcouteurSession implements HttpSessionListener {
+    
+    private static final List SESSIONS = new ArrayList();
 
     @Override
     public void sessionCreated(HttpSessionEvent se) {
-        ServletContext appli = se.getSession().getServletContext();
+        // http://stackoverflow.com/questions/3092363/how-can-i-load-java-httpsession-from-jsessionid/3092495#3092495
+        HttpSession session = se.getSession();
+        ServletContext appli = session.getServletContext();
         int n = Integer.parseInt(appli.getAttribute("nbConnectes").toString());
         n++;
+        SESSIONS.add(session);
         System.out.println(n+"eme Session ouverte");
         appli.setAttribute("nbConnectes", n);
     }
@@ -34,7 +41,12 @@ public class EcouteurSession implements HttpSessionListener {
         ServletContext appli = se.getSession().getServletContext();
         int n = Integer.parseInt(appli.getAttribute("nbConnectes").toString());
         n--;
+        SESSIONS.remove(se.getSession());
         System.out.println((n+1)+"eme Session ferm�e");
         appli.setAttribute("nbConnectes", n);
     }    
+    
+    public static List getAllSessions() {
+        return SESSIONS;
+    }
 }
